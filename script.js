82753cc1a4f6749fd084b02663eb0332604fd159
Adam Szaruga <adamszaruga@gmail.com>
2018-09-20 20:38:04 -0400

    This is my initial commit

---
+++ script.js
@@ -0,0 +1,18 @@
+function marinaraSauce(name) {
+    console.log(`Hey, ${name}!`);
+    console.log("Hey, " + name + "!");
+    console.log('- Heat a medium-large saucepan over medium heat.');
+    console.log('- Add 2 tspns of oil and 5 garlic cloces, cook until golden, about 2 minutes');
+    console.log('- Add 1/4 cup of water, 2 cans of crushed tomatoes, and salt and season with black pepper to taste.');
+    console.log('- Cover the pot, bring toa boil, reduce the heat to medium low, and simmer until the sauce is heated, about 10 minutes');
+    console.log('- Stir in 1/4 cup roughly toa boil ');
+}
+
+
+function enchilladas() {
+    console.log('prep chicken');
+    marinaraSauce("Adam");
+    console.log('other enchillada stuff')
+}
+
+enchilladas();
---
+++ test.js
@@ -0,0 +1,5 @@
+var number = 4;
+
+number = "hi";
+
+console.log(number);
